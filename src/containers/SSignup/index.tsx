7c04eb517a0f0fa455743e1d621eb022621a4595
nguyenminhvuong <minhvuong06082001@gmail.com>
2023-05-02 22:46:54 +0700

    add screnn SSignup

---
+++ src/containers/SSignup/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { View,Text } from "react-native"
+const SSignup = () => {
+    return(
+        <View>
+            <Text>Scren Signup</Text>
+        </View>
+    )
+}
+
+export default SSignup
