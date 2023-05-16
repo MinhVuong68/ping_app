a0809dd6dcd4c597ad20758336e4b6ffcdde7cf2
nguyenminhvuong <minhvuong06082001@gmail.com>
2023-05-16 23:24:01 +0700

    complete enter otp screen

--- src/containers/SLogin/SEnterOTP/index.tsx
+++ src/containers/SLogin/SEnterOTP/index.tsx
@@ -1,15 +1,39 @@
-import React from "react"
-import { View } from "react-native"
-import { Layout } from "../../../theme"
-import { Header, InputOTP } from "../../../components"
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { Layout } from '../../../theme';
+import { Header, Icon, InputOTP } from '../../../components';
+import styles from './styles/SEnterOTPStyle';
+import Color from '../../../theme/Colors';
 
 const SEnterOTP = () => {
-    return (
-        <View style={Layout.full}>
-            <Header title="Nhập mã xác thực" subHeader="Vui lòng không chia sẽ mã xác thực để tránh mất tài khoản"/>
-            <InputOTP/>
+  return (
+    <View style={Layout.full}>
+      <Header
+        title="Nhập mã xác thực"
+        subHeader="Vui lòng không chia sẽ mã xác thực để tránh mất tài khoản"
+      />
+      <View style={[Layout.center, styles.container]}>
+        <View style={Layout.center}>
+          <Icon
+            type="AntDesign"
+            name="message1"
+            size={50}
+            color={Color.primary}
+          />
+          <View style={styles.viewNoti}>
+            <Text style={styles.txtNoti}>
+              Đã gửi mã đến số điện thoại 0899306681
+            </Text>
+            <Text style={styles.txtPlease}>
+              Xin kiểm tra SMS và điền mã xác nhận bên dưới
+            </Text>
+          </View>
         </View>
-    )
-}
+        <InputOTP />
+      </View>
+    </View>
+  );
+};
 
-export default SEnterOTP
+export default SEnterOTP;
---
+++ src/containers/SLogin/SEnterOTP/styles/SEnterOTPStyle.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+import Color from '../../../../theme/Colors';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+  },
+  viewNoti: {
+    marginVertical: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  txtNoti: {
+    fontSize: 15,
+    color: Color.black,
+    fontWeight: 'bold',
+  },
+  txtPlease: {
+    fontSize: 15,
+    color: Color.black,
+  },
+});
+
+export default styles;
